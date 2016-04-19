c5132d8f7b9dd84e73fa28f52a9ceb3551a2ad39
Claudio Matsuoka <cmatsuoka@gmail.com>
2016-04-19 18:38:50 -0300

    Update version and copyright info
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /* Extended Module Player
- * Copyright (C) 1996-2014 Claudio Matsuoka and Hipolito Carraro Jr
+ * Copyright (C) 1996-2016 Claudio Matsuoka and Hipolito Carraro Jr
  *
  * This file is part of the Extended Module Player and is distributed
  * under the terms of the GNU General Public License. See the COPYING
@@ -272,7 +272,7 @@ int main(int argc, char **argv)
 
 	if (opt.verbose > 0) {
 		report("Extended Module Player " VERSION "\n"
-			"Copyright (C) 1996-2014 Claudio Matsuoka and Hipolito Carraro Jr\n");
+			"Copyright (C) 1996-2016 Claudio Matsuoka and Hipolito Carraro Jr\n");
 
 		report("Using %s\n", sound->description);
 
