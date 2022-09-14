9ecd580742871c14d6cf30cd293e5116c86b2468
Ozkan Sezer <sezeroz@gmail.com>
2022-09-14 19:51:37 +0300

    check and conditionally include sys/types.h, sys/time.h, and unistd.h.
    watcom/unistd.h and win32/unistd.h are no longer necessary, especially
    after previous commit.

--- src/watcom/unistd.h
+++
@@ -1,6 +0,0 @@
-#ifndef XMP_WATCOM_UNISTD_H
-#define XMP_WATCOM_UNISTD_H
-
-#include <io.h>  /* do not want Watcom unistd.h */
-
-#endif
--- src/win32/unistd.h
+++
@@ -1,6 +0,0 @@
-#ifndef _XMP_WIN32_UNISTD_H
-#define _XMP_WIN32_UNISTD_H
-
-#include <io.h>
-
-#endif
